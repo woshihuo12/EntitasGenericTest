685ec12e607a872e1e411b276fc792c5283fcb23
huoshijie <huoshijie@bytedance.com>
2022-02-21 12:56:03 +0800

    update logic

--- Entitas.Generic/Core/Contexts.cs
+++ Entitas.Generic/Core/Contexts.cs
@@ -1,36 +1,37 @@
 ﻿using System.Collections.Generic;
+using Entitas;
 using Entitas.Generic;
 
-namespace Entitas
+// namespace Entitas
+// {
+public partial class Contexts
 {
-    public partial class Contexts
+    private List<IContext> _all;
+    public List<IContext> All => _all;
+
+    public void AddScopedContexts()
     {
-        private List<IContext> _all;
-        public List<IContext> All => _all;
-
-        public void AddScopedContexts()
+        if (_all != null)
         {
-            if (_all != null)
-            {
-                return;
-            }
-
-            _all = new List<IContext>(Scopes.Count);
-
-            for (var i = 0; i < Scopes.Count; i++)
-            {
-                _all.Add(Scopes.CreateContext[i].Invoke());
-            }
+            return;
         }
 
-        public ScopedContext<TScope> Get<TScope>() where TScope : IScope
-        {
-            return (ScopedContext<TScope>) _all[Lookup<TScope>.Id];
-        }
+        _all = new List<IContext>(Scopes.Count);
 
-        public IContext Get(int index)
+        for (var i = 0; i < Scopes.Count; i++)
         {
-            return _all[index];
+            _all.Add(Scopes.CreateContext[i].Invoke());
         }
     }
+
+    public ScopedContext<TScope> Get<TScope>() where TScope : IScope
+    {
+        return (ScopedContext<TScope>) _all[Lookup<TScope>.Id];
+    }
+
+    public IContext Get(int index)
+    {
+        return _all[index];
+    }
 }
+// }
